83ca607529af6c61b66c5adc76a9ab03ba80a43a
Yehor Smoliakov <yehors+gh@ukr.net>
2020-10-23 00:00:19 +0300

    Save generated YAML content into a file

--- build.py
+++ build.py
@@ -1,4 +1,20 @@
 from source.project import spec
 
+
+def save(data):
+    with open('api.yaml', 'w') as f:
+        f.write(data)
+
+
 if __name__ == '__main__':
-    print(spec.to_yaml())
+    yaml = spec.to_yaml()
+
+    print(yaml)
+
+    save(yaml)
+
+    print()
+    print()
+    print()
+
+    print('The content was saved into the api.yaml file!')
